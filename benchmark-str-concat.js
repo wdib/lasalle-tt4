04d45e0d29972797e4193698e67111ce195a2c55
William Dib <info@williamdib.com>
2025-01-19 19:47:09 -0500

    Added benchmark-str-concat.js

---
+++ benchmark-str-concat.js
@@ -0,0 +1,25 @@
+const
+  strConcatObj     = require( './string-concatenation' ),
+  createMessageAFn = strConcatObj.createMessageAFn,
+  createMessageBFn = strConcatObj.createMessageBFn
+;
+
+// Begin /benchmark A vs. B/
+// Purpose : Compare the performance of functions A and B
+// Actions :
+//   * Start a timer
+//   * Execute the function 1000 times
+//   * Stop the timer
+//
+console.time( 'A' );
+for ( let i = 0; i < 1000; i++ ) {
+  createMessageAFn( 'Hello', 'World' );
+}
+console.timeEnd( 'A' );
+
+console.time( 'B' );
+for ( let i = 0; i < 1000; i++ ) {
+  createMessageBFn( 'Hello', 'World' );
+}
+console.timeEnd( 'B' );
+// . END /benchmark A vs. B/
