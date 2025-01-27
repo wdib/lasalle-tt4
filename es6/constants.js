f5c7cccb4ee360dce1c22e73081ee74494cd848a
William Dib <info@williamdib.com>
2025-01-27 08:40:14 -0500

    Added array const example

--- es6/constants.js
+++ es6/constants.js
@@ -1,5 +1,9 @@
 const pi = 3.14;
 console.log( pi ); // 3.14
 
+const arr = [];
+arr[ 0 ] = 'Sally'; // This is allowed!
+console.log( arr[ 0 ] );
+
 // Uncommenting the following line will cause an error:
 // pi = 3.14159;  // TypeError: Assignment to constant variable.
