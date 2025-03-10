c9f53c0b2b94381e8d26107b5157c4fa1e4fc0d3
William Dib <info@williamdib.com>
2025-03-10 09:12:15 -0400

    Added basic update mode to UserItem component

--- frontend/src/components/UserItem.jsx
+++ frontend/src/components/UserItem.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function UserItem ( { item_map } ) {
+  const [ isUpdateMode, setIsUpdateMode ] = useState( false );
+
+  function handleDoubleClick () {
+    setIsUpdateMode( true );
+  }
+
+  function handleBlur () {
+    setIsUpdateMode( false );
+  }
+
   return (
     <li>
-      <label>{ item_map.name }</label>
+      <label
+        onDoubleClick = { handleDoubleClick }
+      >{ item_map.name }</label>
+      {
+        isUpdateMode &&
+        <input
+          onBlur = { handleBlur }
+          autoFocus
+        />
+      }
     </li>
   );
 }
